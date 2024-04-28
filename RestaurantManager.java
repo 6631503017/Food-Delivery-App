e5bb4cb7a7898e354334805320edff81d5592d59
ABOUTBLANK <6631503017@lamduan.mfu.ac.th>
2024-04-28 22:22:41 +0700

    addition push
    Restaurant Manager addition

--- RestaurantManager.java
+++ RestaurantManager.java
@@ -1,6 +1,56 @@
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 
 public class RestaurantManager {
     
+    private static Gson gson = new Gson();
+
+    public static void exportRestaurantData(ArrayList<Restaurant> restaurants) {
+        String folderName = "Export";
+        String FileName = "export.json";
+
+        try {
+
+            Path directoryPath = Paths.get(folderName);
+            if (!Files.exists(directoryPath)) {
+                Files.createDirectories(directoryPath);
+            }
+        
+            try (BufferedWriter export = new BufferedWriter(new FileWriter(Paths.get(folderName, FileName).toString()))) {
+                String jsonString = gson.toJson(restaurants);
+                export.write(jsonString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the directory.");
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Restaurant> importRestaurantData() {
+        String folderName = "Import";
+        String FileName = "export.json";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(Paths.get(folderName, FileName).toString()))) {
+            ArrayList<Restaurant> restaurants = gson.fromJson(reader, new TypeToken<ArrayList>(){}.getType());
+            return restaurants;
+
+        } catch (IOException e) {
+            System.out.println("Error importing restaurant data from JSON: " + e.getMessage());
+            return null;
+        }
+    }
 }
